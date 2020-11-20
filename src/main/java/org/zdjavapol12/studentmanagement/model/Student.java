05c5c899232ace865f48e7f4424393a9206874db
Robert Forynski <forynski@gmail.com>
2020-11-20 10:49:25 +0000

    added student model

--- src/main/java/org/zdjavapol12/studentmanagement/model/Student.java
+++ src/main/java/org/zdjavapol12/studentmanagement/model/Student.java
@@ -1,4 +1,23 @@
 package org.zdjavapol12.studentmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String fieldOfStudy;
+    private Integer mobile;
+    private LocalDate startDate;
+    private LocalDate graduationDate;
+
 }
